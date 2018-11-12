1b105ec2b8d48962f88fe520902671f386d1134f
root <root@raspberrypi>
2018-11-12 11:42:03 +0100

    fix SchalterTestter.py

--- PythonScripts/SchalterTest.py
+++ PythonScripts/SchalterTest.py
@@ -1,34 +1,39 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 i = True
+t =1
 
 def button_callback(channel):
     global i
-    if i:
-        f = open("/home/pi/NetBeansProjects/LUMA/PythonScripts/KnopfDruck.txt", "w")
-        f.write("1")
-        f.close()
-        i = False
-        print("1")
-        time.sleep(5)
-        print("rdy")
+    global t
+    if GPIO.input(24) == 1:
+        if i:
+            f = open("/home/pi/NetBeansProjects/LUMA/PythonScripts/KnopfDruck.txt", "w")
+            f.write("1")
+            f.close()
+            i = False
+            print(i)
+            time.sleep(t)
+            
+        elif not i:
+            f = open("/home/pi/NetBeansProjects/LUMA/PythonScripts/KnopfDruck.txt", "w")
+            f.write("0")
+            f.close()
+            i = True
+            print (i)
+            time.sleep(t)
+    else:
+        print("7")
         
-    elif not i:
-        f = open("/home/pi/NetBeansProjects/LUMA/PythonScripts/KnopfDruck.txt", "w")
-        f.write("0")
-        f.close()
-        i = True
-        print ("0")
-        time.sleep(5)
-        print("rdy")
-        
+    print("rdy")
 
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(24,GPIO.RISING,callback=button_callback)
+GPIO.add_event_detect(24,GPIO.BOTH,callback=button_callback, bouncetime=300)  
 
-message = input("Press enter to quit \n\n")
+input("Press enter to quit \n\n")
 
 GPIO.cleanup()
